528e16c2a37c6d3f8f5dc1e20d7d3b7462a9f648
sin0713 <narisin1124@gmail.com>
2021-10-06 09:54:29 +0000

    [Add]homes_controller books_controller users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/index'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'homes/top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
