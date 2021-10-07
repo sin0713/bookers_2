2a9688b1d1ae1a8a8ac2e628faacda50fbb9b451
sin0713 <narisin1124@gmail.com>
2021-10-07 13:08:42 +0000

    [Add]Books_index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+
     @book = Book.new
 
     @user = User.find(current_user.id)
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '75a768dd8ef0ecf1c6dba3a2615c9761460e6e6cfd95bf3f4147bab82fe1d687fc9c7edb04b01adfcb18c86fee58a4cd8da42f772ec610a6c5f0b8d89dc6613f'
