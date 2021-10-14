b58fd49c16c506c56eee741b5b00ad470762cd2c
sin0713 <narisin1124@gmail.com>
2021-10-14 07:31:19 +0000

    [Add]コメントエラーメッセージ

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,8 +4,15 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
-    @book_comment.save
-    redirect_to book_path(@book)
+
+     if @book_comment.save
+       flash[:notice] = "Comment has created successfully"
+       redirect_to book_path(@book)
+     else
+       @new_book = Book.new
+       @user = User.find(@book.user_id)
+       render 'books/show'
+     end
   end
 
   def destroy
