6fddfb28f78373677d66eab2c116b6b4da09b3df
sin0713 <narisin1124@gmail.com>
2021-10-14 00:25:42 +0000

    [Add]Comment機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
 
     @user = User.find(@book.user_id)
+    @book_comment = BookComment.new
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
 
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
+  end 
 
   resources :users, only: [:index, :show, :edit, :update]
 
