2a84ca54fb52cba5295a28f982d88458bf02f8df
sin0713 <narisin1124@gmail.com>
2021-10-21 04:04:50 +0000

    [Add]検索機能

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,2 +1,40 @@
 class SearchsController < ApplicationController
+  def search
+    @model = params["model"]
+
+    @method = params["method"]
+
+    @content = params["content"]
+
+    @records = search_for(@model, @content, @method)
+
+  end
+
+  private
+
+
+    def search_for(model, content, method)
+      if model == 'user'
+        if method == 'perfect'
+          User.where(name: content)
+        elsif method == 'pertial'
+          User.where('name LIKE ?', '%'+content+'%')
+        elsif method == 'prefix'
+          User.where('name LIKE ?', content+'%' )
+        else
+           User.where('name LIKE ?', '%'+content )
+        end
+
+      elsif model == 'book'
+        if method == 'perfect'
+          Book.where(title: content)
+        elsif method == 'pertial'
+          Book.where('title LIKE ?', '%'+content+'%')
+        elsif method == 'prefix'
+          Book.where('title LIKE ?', content+'%' )
+        else
+          Book.where('title LIKE ?', '%'+content )
+        end
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/search' => 'searchs#search'
   devise_for :users
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
