aa829d20319fdc4b0ebb6547dc2bc79929c21e89
sin0713 <narisin1124@gmail.com>
2021-11-01 12:23:43 +0000

    [Add]errorメッセージの非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,25 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-
     @book = Book.find(params[:book_id])
-
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
-    @book_comments = @book.book_comments
-
-     if @book_comment.save
-       flash[:notice] = "Comment has created successfully"
-     else
-       @new_book = Book.new
-       @user = User.find(@book.user_id)
-       render 'books/show'
+     unless @book_comment.save
+       render 'error'
      end
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     @book_comments = BookComment.where(book_id: params[:book_id])
-    BookComment.find_by(id: params[:id]).destroy
+    @book_comments.find_by(id: params[:id]).destroy
   end
 
 
