21253f3f962498bf9dc0f55b568041217e73bece
sin0713 <narisin1124@gmail.com>
2021-11-03 08:09:36 +0000

    前日比と前週比の表作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,25 @@ class UsersController < ApplicationController
 
 
   def show
-    @day_book = @books.created_today
-    
     @user = User.find(params[:id])
     @books = Book.where(user_id: @user.id) #もしくは @books = @user.books なぜならテーブル同士アソシエーションで関連付けが行われているか
     @book = Book.new
-    
-    
+    @today_books = @books.created_today
+    @yesterday_books = @books.created_yesterday
+    @this_week_books = @books.created_this_week
+    @last_week_books = @books.created_last_week
+
+    begin
+      @day_before_ratio = (@today_books.count / @yesterday_books.count) * 100
+    rescue
+      @day_before_ratio = "--%"
+    end
+
+    begin
+      @week_before_ratio = (@this_week_books.count / @last_week_books.count) * 100
+    rescue
+      @week_before_ratio = "--%"
+    end
   end
 
   def edit
@@ -32,19 +44,18 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
     @book = Book.new
   end
-  
+
   def followings
     user = User.find(params[:id])
     @users = user.followings
-  end 
-  
+  end
+
   def followers
     user = User.find(params[:id])
     @users = user.followers
-  end 
+  end
 
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   has_many :relationships, foreign_key: :follow_id
   has_many :followings, through: :relationships, source: :followed
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :followed_id
   has_many :followers, through: :reverse_of_relationships, source: :follow
-  
+
   def is_followed_by?(user)
     reverse_of_relationships.find_by(follow_id: user.id).present?
   end
-  
+
   attachment :profile_image
 
   validates :name, uniqueness: true, length: { in: 2..20 }
