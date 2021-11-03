85204abc899d41fd91d144a5039fdbe94aa7d224
sin0713 <narisin1124@gmail.com>
2021-11-03 10:02:27 +0000

    [Add]InUserController set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,25 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, except: [:index]
 
   def show
-    @user = User.find(params[:id])
     @books = Book.where(user_id: @user.id) #もしくは @books = @user.books なぜならテーブル同士アソシエーションで関連付けが行われているか
     @book = Book.new
-    @today_books = @books.created_today
-    @yesterday_books = @books.created_yesterday
-    @this_week_books = @books.created_this_week
-    @last_week_books = @books.created_last_week
 
     begin
-      @day_before_ratio = (@today_books.count / @yesterday_books.count) * 100
+      @day_before_ratio = (@books.created_today.count / @bookscreated_yesterday.count) * 100
     rescue
       @day_before_ratio = "--%"
     end
 
     begin
-      @week_before_ratio = (@this_week_books.count / @last_week_books.count) * 100
+      @week_before_ratio = (@books.created_this_week.count / @books.created_last_week.count) * 100
     rescue
       @week_before_ratio = "--%"
     end
   end
 
   def edit
-    @user = User.find(params[:id])
     if current_user.id != @user.id
       flash[:alert] = "You have no authority to access the page."
       redirect_to user_path(current_user)
@@ -32,7 +27,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
@@ -48,13 +42,11 @@ class UsersController < ApplicationController
   end
 
   def followings
-    user = User.find(params[:id])
-    @users = user.followings
+    @users = @user.followings
   end
 
   def followers
-    user = User.find(params[:id])
-    @users = user.followers
+    @users = @user.followers
   end
 
 
@@ -62,6 +54,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,17 +1,16 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
 
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  validates :title, presence: true
-  validates :body, presence: true, length: { maximum: 200 }
-
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
   scope :created_this_week, -> { where(created_at: Time.zone.now.all_week) }
   scope :created_last_week, -> { where(created_at: 1.week.ago.all_week) }
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
